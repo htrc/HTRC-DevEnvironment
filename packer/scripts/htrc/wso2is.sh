fa18c6cffb8d70dbf441e269cce529dc5e77f94d
Milinda Pathirage <milinda.pathirage@gmail.com>
2017-02-16 00:09:56 -0500

    wso2is init script

--- packer/scripts/htrc/wso2is.sh
+++ packer/scripts/htrc/wso2is.sh
@@ -3,3 +3,27 @@
 wget -P /tmp/ http://analytics.hathitrust.org/files/wso2is-${WSO2IS_VERSION}.zip
 unzip /tmp/wso2is-${WSO2IS_VERSION}.zip -d /usr/share
 ln -s /usr/share/wso2is-${WSO2IS_VERSION} /usr/share/wso2is
+
+cat > /etc/default/wso2is <<EOF
+JAVA_HOME=/usr/java/default
+CARBON_HOME=/usr/share/wso2is
+EOF
+
+cat > /etc/systemd/system/wso2is.service <<EOF
+[Unit]
+Description=WSO2 Identity Server service
+Wants=syslog.target network.target
+After=syslog.target network.target
+
+[Service]
+WorkingDirectory=/usr/share/wso2is
+EnvironmentFile=-/etc/default/wso2is
+RestartForceExitStatus=121
+ExecStart=/usr/share/wso2is/bin/wso2server.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable wso2is.service
