39971d2f1283f09f9a2efe818c23c76ff04bb953
Jim Lambert <jim.lambert@gmail.com>
2017-04-12 09:50:56 -0400

    fixing no clobber on wgets

--- scripts/install-prerequisites.sh
+++ scripts/install-prerequisites.sh
@@ -2,13 +2,13 @@
 yum -y install wget
 cd /tmp
 
- wget -P /tmp/ --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm"
+wget -nc -P /tmp/ --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm"
 
- yum -y install /tmp/jdk-8u121-linux-x64.rpm
+yum -y install /tmp/jdk-8u121-linux-x64.rpm
 
- rm /tmp/jdk-8u121-linux-x64.rpm
+rm /tmp/jdk-8u121-linux-x64.rpm
 
-wget -P /tmp/ https://repo.mysql.com//mysql57-community-release-el7-9.noarch.rpm
+wget -nc -P /tmp/ https://repo.mysql.com//mysql57-community-release-el7-9.noarch.rpm
 
 rpm -ivh /tmp/mysql57-community-release-el7-9.noarch.rpm
 
