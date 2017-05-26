fa9e9f4e4deba89dc9174b32a8cac050ab6e6750
Milinda Pathirage <milinda.pathirage@gmail.com>
2017-05-26 11:50:08 -0400

    JDK version update.

--- scripts/install-prerequisites.sh
+++ scripts/install-prerequisites.sh
@@ -2,11 +2,11 @@
 yum -y install wget
 cd /tmp
 
-wget -nc -P /tmp/ --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm"
+wget -nc -P /tmp/ --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 
-yum -y install /tmp/jdk-8u121-linux-x64.rpm
+yum -y install /tmp/jdk-8u131-linux-x64.rpm
 
-rm /tmp/jdk-8u121-linux-x64.rpm
+rm /tmp/jdk-8u131-linux-x64.rpm
 
 wget -nc -P /tmp/ https://repo.mysql.com//mysql57-community-release-el7-9.noarch.rpm
 
