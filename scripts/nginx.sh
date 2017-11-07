fb535cba9aa1d1a2d03c86b9042a74c9bf183924
Boris Capitanu <borice@hotmail.com>
2017-11-06 18:43:51 -0600

    Updated nginx configuration

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -eux
 
 # Copy sites-available and nginx.conf from configurations
-cp -r /devenv_configurations/nginx/sites-available /etc/nginx/
+cp -R /devenv_configurations/nginx/sites-available /etc/nginx/
 cp /devenv_configurations/nginx/nginx.conf /etc/nginx/
 
-# Copy certs
-mkdir /etc/nginx/certs
-cp -r /devenv_certs/* /etc/nginx/certs
-
 mkdir /etc/nginx/sites-enabled
-ln -s /etc/nginx/sites-available/is-notls.conf /etc/nginx/sites-enabled/is-notls.conf
-ln -s /etc/nginx/sites-available/email-notls.conf /etc/nginx/sites-enabled/email-notls.conf
-ln -s /etc/nginx/sites-available/regex-notls.conf /etc/nginx/sites-enabled/regex-notls.conf
-ln -s /etc/nginx/sites-available/ag.conf /etc/nginx/sites-enabled/ag.conf
+ln -s /etc/nginx/sites-available/idp.conf /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/email.conf /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/registry.conf /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/ag.conf /etc/nginx/sites-enabled
 chown -R vagrant:root /etc/nginx
+
+# Give NGINX access to certificates in /etc/pki/CA
+usermod -a -G certs nginx
+
