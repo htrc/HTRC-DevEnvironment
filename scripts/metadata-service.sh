96da01dbb10fd83fce1c37d3cc6a64affb3b6b3a
Boris Capitanu <borice@hotmail.com>
2017-12-01 15:16:23 -0600

    Added systemd service definition

--- scripts/metadata-service.sh
+++ scripts/metadata-service.sh
@@ -1,14 +1,29 @@
 #!/bin/sh -eux
-ls -l /
-ls -l /devenv_sources
 cd /devenv_sources/HTRC-MetadataService
 
 cat << EOF > start.sh
 #!/bin/sh
 export METASERVICE_SECRET="k5@k3G@u8qh>dOJUIRypOyzgKHA;oXZ^ShBLny8s;w7pvwvalBR8Q]<gNENrrVWB"
-./target/universal/stage/bin/htrc-metadataservice -Dhttp.address=localhost -Dhttp.port=8083 -Dplay.http.context=/api
+/devenv_sources/HTRC-MetadataService/target/universal/stage/bin/htrc-metadataservice -Dhttp.address=localhost -Dhttp.port=8083 -Dplay.http.context=/api
 EOF
 
 chmod +x start.sh
+sbt stage
 
-sbt stage && tmux new -n start -s metadata ./start.sh \; detach
+# Set up the systemd service
+cat << EOF > /etc/systemd/system/metadata.service
+[Unit]
+Description=HTRC Metadata Service
+Wants=syslog.target network.target
+After=syslog.target network.target mongod.service
+
+[Service]
+WorkingDirectory=/devenv_sources/HTRC-MetadataService
+ExecStart=/devenv_sources/HTRC-MetadataService/start.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable metadata.service --now
